29c522aa3f0981c232596ee5e017caae594702a0
Nick Luong <nckluong@gmail.com>
2018-10-14 22:11:00 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+
+function Driver(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+function PickupLocation(a, c) {
+  this.address = a;
+  this.city = c;
+}
